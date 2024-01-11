4eb90994bc1b956da17a619b02b51a844622bcd7
HP <you@example.com>
2024-01-11 19:55:48 +0300

    A new files is added

---
+++ rectangles/rectangle2.py
@@ -0,0 +1,9 @@
+from rectangle import Rectangle
+
+r1 = Rectangle(10, 5)
+
+print("r1.getWidth=", r1.getWidth())
+print("r1.getArea=", r1.getArea())
+
+
+
---
+++ rectangles/rectangle3.py
@@ -0,0 +1,7 @@
+from rectangle import Rectangle
+
+r2 = Rectangle(20, 10)
+
+print("r2.getWidth=", r2.getWidth())
+print("r2.getHeight=", r2.getHeight())
+print("r2.getArea=", r2.getArea())
