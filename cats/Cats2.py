bd453254586268ed7821a260854b308caa65ab74
HP <you@example.com>
2024-01-11 19:50:23 +0300

    A new files is added

---
+++ cats/Cats2.py
@@ -0,0 +1,11 @@
+from Cats import Cat
+
+cat1 = Cat("Барон", "мальчик", 2)
+
+# print("cat1.name=", cat1.name)
+# print("cat1.gender=", cat1.gender)
+# print("cat1.age=", cat1.age)
+print("Имя=", cat1.getName(), "Пол=", cat1.getGender(), "Возраст=", cat1.getAge())
+
+cat2 = Cat("Сэм", "мальчик", 2)
+print("Имя=", cat2.getName(), "Пол=", cat2.getGender(), "Возраст=", cat2.getAge())
